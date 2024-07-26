144e3a1280d2354bc68c85b27c758b833f8d0aae
Kellen Pinho <kellenraissarbp@gmail.com>
2024-07-25 21:49:06 -0300

    first commit

---
+++ src/services/PessoaServices.js
@@ -0,0 +1,36 @@
+const dataSource = require('../database/models');
+const Services = require('./Services.js');
+
+class PessoaServices extends Services {
+  constructor() {
+    super('Pessoa');
+    this.matriculaServices = new Services('Matricula');
+  }
+
+  async pegaMatriculasAtivasPorEstudante(id) {
+    const estudante = await super.pegaUmRegistroPorId(id);
+    const listaMatriculas = await estudante.getAulasMatriculadas();
+    return listaMatriculas;
+  }
+
+  async pegaTodasAsMatriculasAtivasPorEstudante(id) {
+    const estudante = await super.pegaUmRegistroPorId(id);
+    const listaMatriculas = await estudante.todasAsMatriculas();
+    return listaMatriculas;
+  }
+
+  async pegaPessoasEscopoTodos () {
+    const listaPessoas = await super.pegaRegistrosPorEscopo('todosOsRegistros');
+
+    return listaPessoas;
+  }
+
+  async cancelaPessoaEMatriculas(estudante_id) {
+    return dataSource.sequelize.transaction(async (transacao) => {
+      await super.atualizaRegistro({ ativo: false }, {id: estudante_id}, transacao);
+      await this.matriculaServices.atualizaRegistro({status: 'cancelado'}, {estudante_id: estudante_id}, transacao);
+    });
+  }
+}
+
+module.exports = PessoaServices;
